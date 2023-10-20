0491d4d34651a1bca7a19f4ea10f3d81b0563409
jennifer sammy <jensam209@gmail.com>
2023-10-20 14:29:50 +0300

    add script.ts

--- script.js
+++ script.js
@@ -1,64 +1,57 @@
-//selecting elements
-const form = document.querySelector("#form");
-const email = document.querySelector("#input-email");
-const errorSpan = document.querySelector("#span-error");
-const signUp = document.querySelector("#sign-up-div");
-const successfull = document.querySelector("#successfull-div");
-const emailSpan = document.querySelector("#email-address-span");
-const dismissButton = document.querySelector("#dismiss-button");
-
-//prevent default form submit
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  //trim,check if email input is empty,call errorFUNC,validate,calls successFunc
-  let emailValue = email.value.trim();
-
-  if (emailValue === "") {
-    errorFunc("Email required");
-  } else if (!validateEmail(emailValue)) {
-    errorFunc("Valid email required");
-  } else {
-    successFunc();
-  }
-});
-
-
-//event listener hide successfull div
-dismissButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  signUp.style.display = "flex";
-  successfull.style.display = "none";
-});
-
-
-//handle error message
-function errorFunc(message) {
-  errorSpan.style.display = "block";
-  errorSpan.innerText = message;
-  email.classList.add("error");
-  email.style.border = "solid 1px hsl(4, 100%, 67%)";
-}
-
-//handle style to typing
-function typing() {
-  errorSpan.style.display = "none";
-  email.style.border = "solid 1px hsl(231, 7%, 60%)";
-  if (email.classList.contains("error")) {
-    email.classList.remove("error");
-  }
-}
-
-//handle successful submission
-function successFunc() {
-  let emailValue = email.value.trim();
-  emailSpan.innerText = emailValue;
-  signUp.style.display = "none";
-  successfull.style.display = "block";
-}
-
-
-//validates email format
-function validateEmail(email) {
-  return /\S+@\S+\.\S+/.test(email);
+var form = document.querySelector("#form");
+var email = document.querySelector("#input-email");
+var errorSpan = document.querySelector("#span-error");
+var signUp = document.querySelector("#sign-up-div");
+var successfull = document.querySelector("#successfull-div");
+var emailSpan = document.querySelector("#email-address-span");
+var dismissButton = document.querySelector("#dismiss-button");
+if (form && email && errorSpan && signUp && successfull && emailSpan && dismissButton) {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        var emailValue = email.value.trim();
+        if (emailValue === "") {
+            errorFunc("Email required");
+        }
+        else if (!validateEmail(emailValue)) {
+            errorFunc("Valid email required");
+        }
+        else {
+            successFunc(email);
+        }
+    });
+    dismissButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        if (signUp && successfull) {
+            signUp.style.display = "flex";
+            successfull.style.display = "none";
+        }
+    });
+    function errorFunc(message) {
+        if (errorSpan && email) {
+            errorSpan.style.display = "block";
+            errorSpan.innerText = message;
+            email.classList.add("error");
+            email.style.border = "solid 1px hsl(4, 100%, 67%)";
+        }
+    }
+    function typing() {
+        if (errorSpan && email) {
+            errorSpan.style.display = "none";
+            email.style.border = "solid 1px hsl(231, 7%, 60%)";
+            if (email.classList.contains("error")) {
+                email.classList.remove("error");
+            }
+        }
+    }
+    function successFunc(email) {
+        var emailValue = email.value.trim();
+        if (emailSpan && signUp && successfull) {
+            emailSpan.innerText = emailValue;
+            signUp.style.display = "none";
+            successfull.style.display = "block";
+        }
+    }
+    function validateEmail(email) {
+        return /\S+@\S+\.\S+/.test(email);
+    }
 }
---
+++ script.ts
@@ -0,0 +1,68 @@
+const form = document.querySelector<HTMLFormElement>("#form");
+const email = document.querySelector<HTMLInputElement>("#input-email");
+const errorSpan = document.querySelector<HTMLSpanElement>("#span-error");
+const signUp = document.querySelector<HTMLDivElement>("#sign-up-div");
+const successfull = document.querySelector<HTMLDivElement>("#successfull-div");
+const emailSpan = document.querySelector<HTMLSpanElement>(
+  "#email-address-span"
+);
+const dismissButton =
+  document.querySelector<HTMLButtonElement>("#dismiss-button");
+
+if (
+  form && email && errorSpan && signUp && successfull && emailSpan && dismissButton
+) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let emailValue: string = email.value.trim();
+
+    if (emailValue === "") {
+      errorFunc("Email required");
+    } else if (!validateEmail(emailValue)) {
+      errorFunc("Valid email required");
+    } else {
+      successFunc(email);
+    }
+  });
+
+  dismissButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (signUp && successfull) {
+      signUp.style.display = "flex";
+      successfull.style.display = "none";
+    }
+  });
+
+  function errorFunc(message: string) {
+    if (errorSpan && email) {
+      errorSpan.style.display = "block";
+      errorSpan.innerText = message;
+      email.classList.add("error");
+      email.style.border = "solid 1px hsl(4, 100%, 67%)";
+    }
+  }
+
+  function typing() {
+    if (errorSpan && email) {
+      errorSpan.style.display = "none";
+      email.style.border = "solid 1px hsl(231, 7%, 60%)";
+      if (email.classList.contains("error")) {
+        email.classList.remove("error");
+      }
+    }
+  }
+
+  function successFunc(email: HTMLInputElement) {
+    let emailValue: string = email.value.trim();
+    if (emailSpan && signUp && successfull) {
+      emailSpan.innerText = emailValue;
+      signUp.style.display = "none";
+      successfull.style.display = "block";
+    }
+  }
+
+  function validateEmail(email: string): boolean {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+}
