823559ba79d1b9441804b60d59478fae244aa1c5
jennifer sammy <jensam209@gmail.com>
2023-10-21 17:49:29 +0300

    add script.js OOP

--- script.js
+++ script.js
@@ -1,57 +1,82 @@
-var form = document.querySelector("#form");
-var email = document.querySelector("#input-email");
-var errorSpan = document.querySelector("#span-error");
-var signUp = document.querySelector("#sign-up-div");
-var successfull = document.querySelector("#successfull-div");
-var emailSpan = document.querySelector("#email-address-span");
-var dismissButton = document.querySelector("#dismiss-button");
-if (form && email && errorSpan && signUp && successfull && emailSpan && dismissButton) {
-    form.addEventListener("submit", function (e) {
+class FormHandler {
+  constructor() {
+    this.form = document.querySelector("#form");
+    this.email = document.querySelector("#input-email");
+    this.errorSpan = document.querySelector("#span-error");
+    this.signUp = document.querySelector("#sign-up-div");
+    this.successfull = document.querySelector("#successfull-div");
+    this.emailSpan = document.querySelector("#email-address-span");
+    this.dismissButton = document.querySelector("#dismiss-button");
+
+    if (
+      this.form &&
+      this.email &&
+      this.errorSpan &&
+      this.signUp &&
+      this.successfull &&
+      this.emailSpan &&
+      this.dismissButton
+    ) {
+      this.form.addEventListener("submit", (e) => {
         e.preventDefault();
-        var emailValue = email.value.trim();
+        let emailValue = this.email.value.trim();
         if (emailValue === "") {
-            errorFunc("Email required");
+          this.errorFunc("Email required");
+        } else if (!this.validateEmail(emailValue)) {
+          this.errorFunc("Valid email required");
+        } else {
+          this.successFunc(this.email);
         }
-        else if (!validateEmail(emailValue)) {
-            errorFunc("Valid email required");
-        }
-        else {
-            successFunc(email);
-        }
-    });
-    dismissButton.addEventListener("click", function (e) {
+      });
+
+      this.dismissButton.addEventListener("click", (e) => {
         e.preventDefault();
-        if (signUp && successfull) {
-            signUp.style.display = "flex";
-            successfull.style.display = "none";
+        if (this.signUp && this.successfull) {
+          this.signUp.style.display = "flex";
+          this.successfull.style.display = "none";
         }
-    });
-    function errorFunc(message) {
-        if (errorSpan && email) {
-            errorSpan.style.display = "block";
-            errorSpan.innerText = message;
-            email.classList.add("error");
-            email.style.border = "solid 1px hsl(4, 100%, 67%)";
+      });
+
+      this.email.addEventListener("input", () => {
+        this.typing();
+      });
+    }
+  }
+
+  errorFunc(message) {
+    if (this.errorSpan && this.email) {
+      this.errorSpan.style.display = "block";
+      this.errorSpan.innerText = message;
+      this.email.classList.add("error");
+      this.email.style.border = "solid 1px hsl(4, 100%, 67%)";
+    }
+  }
+
+  typing() {
+    if (this.errorSpan && this.email) {
+      let emailValue = this.email.value.trim();
+      if (this.validateEmail(emailValue)) {
+        this.errorSpan.style.display = "none";
+        this.email.style.border = "solid 1px hsl(231, 7%, 60%)";
+        if (this.email.classList.contains("error")) {
+          this.email.classList.remove("error");
         }
+      }
     }
-    function typing() {
-        if (errorSpan && email) {
-            errorSpan.style.display = "none";
-            email.style.border = "solid 1px hsl(231, 7%, 60%)";
-            if (email.classList.contains("error")) {
-                email.classList.remove("error");
-            }
-        }
+  }
+
+  successFunc(email) {
+    let emailValue = email.value.trim();
+    if (this.emailSpan && this.signUp && this.successfull) {
+      this.emailSpan.innerText = emailValue;
+      this.signUp.style.display = "none";
+      this.successfull.style.display = "block";
     }
-    function successFunc(email) {
-        var emailValue = email.value.trim();
-        if (emailSpan && signUp && successfull) {
-            emailSpan.innerText = emailValue;
-            signUp.style.display = "none";
-            successfull.style.display = "block";
-        }
-    }
-    function validateEmail(email) {
-        return /\S+@\S+\.\S+/.test(email);
-    }
+  }
+
+  validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
 }
+
+const formHandler = new FormHandler();
